70a433b2d710e2eb60e25da8f33cbc98a6f3a59f
Carlos Madoery <131295103+Theramon88@users.noreply.github.com>
2023-09-17 22:58:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import pandas as pd
 # Instanciamos FastAPI
 app = FastAPI()
 # Cargamos los archivos a consumir
-df_userdata_1 = pd.read_parquet('Data\\df_userdata_1.parquet')
-df_userdata_2 = pd.read_parquet('Data\\df_userdata_2.parquet')
-df_countreviews = pd.read_parquet('Data\\df_countreviews.parquet')
-df_genres = pd.read_parquet('Data\\df_genres.parquet')
-df_top5 = pd.read_parquet('Data\\df_top5.parquet')
-df_developer = pd.read_parquet('Data\\df_developer.parquet')
-df_sentiment = pd.read_parquet('Data\\df_sentiment.parquet')
+df_userdata_1 = pd.read_parquet('Data/df_userdata_1.parquet')
+df_userdata_2 = pd.read_parquet('Data/df_userdata_2.parquet')
+df_countreviews = pd.read_parquet('Data/df_countreviews.parquet')
+df_genres = pd.read_parquet('Data/df_genres.parquet')
+df_top5 = pd.read_parquet('Data/df_top5.parquet')
+df_developer = pd.read_parquet('Data/df_developer.parquet')
+df_sentiment = pd.read_parquet('Data/df_sentiment.parquet')
 
 @app.get("/")  # decorator to define a route http://127.0.0.1:8000
 def index():
